36d4437c361503c51611aeed558c48a18ac0a013
Yolande Chou <duanyuling000@sina.com>
2017-10-24 14:03:34 +0800

    Update ArticleCom.vue

--- src/components/ArticleCom.vue
+++ src/components/ArticleCom.vue
@@ -7,19 +7,25 @@
     	<span>浏览量：{{ article.visit_count }}</span>
     	<span>来自：{{article.tab}}</span>
     	<h3>{{ article.title }}</h3>
-    	<div class="content" v-html = 'article.content'></div>
-    	<div v-for='(reply,index) of article.replies'>
-             <router-link :to='{name:"UserRoute",params:{name:reply.author.loginname}}'>
-                <img :src="reply.author.avatar_url" :title='reply.author.loginname' />
-             </router-link>
-             <h5>{{ reply.author.loginname }}</h5>
-             <span>{{ replyTime }}</span>
-             <span>
-             	<icon class="thumbs-up" scale='4'></icon>
-             	<span>{{ reply.ups.length }}</span>
-             </span>
-             <p v-html='reply.content'></p>
-    	</div>
+    	<div class="content" id="content" v-html = 'article.content'></div>
+        <div id="reply">
+        	<div v-for='(reply,index) of article.replies' class="reply">
+                 <router-link :to='{name:"UserRoute",params:{name:reply.author.loginname}}'>
+                    <img :src="reply.author.avatar_url" :title='reply.author.loginname' />
+                 </router-link>
+                 <div>
+                     <div class="replyInfo">
+                         <span>{{ reply.author.loginname }}</span>
+                         <span>{{ replyTime }}</span>
+                         <span v-if='reply.ups.length > 0' class="thumbs">
+                         	<icon name="thumbs-up" scale='4'></icon>
+                         	<span>{{ reply.ups.length }}</span>
+                         </span>
+                    </div>
+                     <p v-html='reply.content'></p>
+                </div>
+        	</div>
+        </div>
     </div>
 </template>
 
@@ -48,15 +54,83 @@ export default{
 			return String(this.article.create_at).match(/.{16}/)[0].replace(/.{2}/,'').replace(/[T]/,' ');
 		}
 	},
-	beforeCreated() {
-		this.$http({
-			url:`https://cnodejs.org/api/v1${this.$route.path}`,
-			method:'Get'
-		}).then((res)=>{
-			this.article = res.data.data;
-		}).catch((res)=>{
-			console.log(res);
-		})
-	}
+    watch: {
+        article(val){
+            if(val){
+                this.loading =false;
+            }
+        }
+    },
+	 beforeCreate() {
+        this.$http({
+            url: `https://cnodejs.org/api/v1${this.$route.path}`,
+            method: 'get',
+        }).then((res) => {
+            console.log(111,res);
+            if (res.data.success === true) {
+                this.article = res.data.data;
+                this.$parent.authorName = this.article.author.loginname;
+            } else {
+                this.article = 'Sorry, Something wrong happened when getting the remote data';
+            }
+        }).catch((res) => {
+            console.log('ArticleCom.vue: ', res);
+        });
+    },
 }
 </script>
+<style scoped>
+@import url('../assets/markdowm-github.css');
+.secDiv{
+    width:60%;
+    padding:2rem;
+
+}
+.reply img{
+    width:6rem;
+    height:6rem;
+}
+#content {
+    margin: 2rem auto 2rem auto;
+    padding: 2rem 1rem 2rem 1rem;
+    border: 1px solid #ddd;
+    line-height: 1.6;
+    padding-bottom: 1rem;
+    background: #EFF2F7;
+}
+
+#reply {
+    display: flex;
+    flex-direction: column;
+}
+
+#reply img {
+    width: 5rem;
+    height: 5rem;
+    margin-right: 1rem;
+}
+.reply{
+    display:flex;
+    padding:2rem 0;
+    border-bottom:1px solid #ccc;
+}
+.reply>div {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    margin-left: 1rem;
+    justify-content: space-around;
+}
+.replyInfo,.reply p{
+    width:100%;
+}
+.replyInfo>span{
+    font-size:0.8rem;
+    color:#999;
+    margin-right:2rem;
+}
+.replyInfo .thumbs{
+    float:right;
+}
+</style>
